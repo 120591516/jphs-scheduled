e70b2cb7f1a64d64653a0774a44e84893bfeaaa9
120591516 <120591516@qq.com>
2017-09-21 12:03:44 +0800

    asdf

--- src/main/java/com/jinpaihushi/JphsScheduledApplication.java
+++ src/main/java/com/jinpaihushi/JphsScheduledApplication.java
@@ -2,11 +2,13 @@ package com.jinpaihushi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class JphsScheduledApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JphsScheduledApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JphsScheduledApplication.class, args);
+    }
 }
--- src/main/java/com/jinpaihushi/nurserank/service/NurseRankScheduled.java
+++ src/main/java/com/jinpaihushi/nurserank/service/NurseRankScheduled.java
@@ -1,10 +1,11 @@
 package com.jinpaihushi.nurserank.service;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.transaction.Transactional;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.collections.CollectionUtils;
@@ -19,13 +20,13 @@ import com.jinpaihushi.jphs.nurse.model.NurseRank;
 import com.jinpaihushi.jphs.nurse.service.NurseRankService;
 import com.jinpaihushi.jphs.order.model.Order;
 import com.jinpaihushi.jphs.order.service.OrderService;
+import com.jinpaihushi.utils.CycleTimeUtils;
 import com.jinpaihushi.utils.MyPredicate;
 
 @Component
 @Configurable
 @EnableScheduling
 public class NurseRankScheduled {
-    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
     private OrderService orderService;
@@ -34,34 +35,37 @@ public class NurseRankScheduled {
     private NurseRankService nurseRankService;
 
     @Scheduled(cron = "${test}")
+    @Transactional
     public void updateNurseRank() {
         try {
-            String dateStr = sdf.format(new Date());
+            String dateStr = CycleTimeUtils.getPastDate(2);
             Map<String, Object> query = new HashMap<>();
             query.put("confirmTime", dateStr);
             query.put("schedule", 5);
             //获取昨天已完成的所有订单
             List<Order> orderList = orderService.getCompletedOrder(query);
             //获取护士列表中的所有的数据
-            NurseRank nurseRankQuery = new NurseRank();
-            nurseRankQuery.setStatus(1);
-            List<NurseRank> nurseList = nurseRankService.list(nurseRankQuery);
-            for (NurseRank nurseRank : nurseList) {
-                nurseRankQuery = new NurseRank();
-                //生成一个0-3的随机数
-                int num = (int) (Math.random() * 4);
-                Predicate predicate = new MyPredicate("acceptUserId", nurseRank.getUserId());
-                @SuppressWarnings("unchecked")
-                List<Order> select = (List<Order>) CollectionUtils.select(orderList, predicate);
-                //更新护士的排序
-                //昨天获得的分数
-                int degreeHeat = select.size() * 5;
-                BeanUtils.copyProperties(nurseRankQuery, nurseRank);
-                nurseRankQuery.setBaseServerNumber(nurseRank.getBaseServerNumber() + num);
-                nurseRankQuery.setDegreeHeat(nurseRank.getDegreeHeat() + degreeHeat);
-                nurseRankQuery.setRealServerNumer(nurseRank.getRealServerNumer() + select.size());
-                nurseRankQuery.setUpdateTime(new Date());
-                nurseRankService.update(nurseRankQuery);
+            if (orderList.size() > 0) {
+                NurseRank nurseRankQuery = new NurseRank();
+                nurseRankQuery.setStatus(1);
+                List<NurseRank> nurseList = nurseRankService.list(nurseRankQuery);
+                for (NurseRank nurseRank : nurseList) {
+                    nurseRankQuery = new NurseRank();
+                    //生成一个0-3的随机数
+                    int num = (int) (Math.random() * 4);
+                    Predicate predicate = new MyPredicate("acceptUserId", nurseRank.getUserId());
+                    @SuppressWarnings("unchecked")
+                    List<Order> select = (List<Order>) CollectionUtils.select(orderList, predicate);
+                    //更新护士的排序
+                    //昨天获得的分数
+                    int degreeHeat = select.size() * 5;
+                    BeanUtils.copyProperties(nurseRankQuery, nurseRank);
+                    nurseRankQuery.setBaseServerNumber(nurseRank.getBaseServerNumber() + num);
+                    nurseRankQuery.setDegreeHeat(nurseRank.getDegreeHeat() + degreeHeat);
+                    nurseRankQuery.setRealServerNumer(nurseRank.getRealServerNumer() + select.size());
+                    nurseRankQuery.setUpdateTime(new Date());
+                    nurseRankService.update(nurseRankQuery);
+                }
             }
 
         }
